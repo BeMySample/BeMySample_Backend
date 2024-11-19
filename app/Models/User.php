8686164fb2481dbea8f75bdd9fe21602650c8746
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2024-11-19 12:45:03 +0700

    feat: Add Auth Google and Integrate with FE

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,34 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
-    protected $table = 'user';
+    use HasApiTokens, Notifiable;
 
     protected $fillable = [
         'username',
         'nama_lengkap',
         'email',
+        'google_id',
+        'avatar',
         'password',
-        'tanggal_lahir',
-        'jenis_kelamin',
-        'umur',
-        'lokasi',
-        'minat',
-        'institusi',
-        'poin_saya',
-        'pekerjaan',
-        'profilepic'
     ];
 
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = bcrypt($password);
-    }
+    protected $table = 'users'; // Default Laravel
 }
