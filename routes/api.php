f7b96070d32680b68a89c9ae8c09add28341725c
rezkysantika <rezkysantika21@gmail.com>
2024-12-05 00:19:53 +0700

    api for delete

--- routes/api.php
+++ routes/api.php
@@ -36,46 +36,46 @@ Route::get('users', [UserController::class, 'index']);
 Route::post('users', [UserController::class, 'store']);
 Route::get('users/{id}', [UserController::class, 'show']);
 Route::put('users/{id}', [UserController::class, 'update']);
-Route::delete('users/{id}', [UserController::class, 'destroy']);
+Route::delete('users/delete/{id}', [UserController::class, 'destroy']);
 
 Route::get('jawaban-soal', [JawabanSoalController::class, 'index']);
 Route::post('jawaban-soal', [JawabanSoalController::class, 'store']);
 Route::get('jawaban-soal/{id}', [JawabanSoalController::class, 'show']);
 Route::put('jawaban-soal/{id}', [JawabanSoalController::class, 'update']); 
-Route::delete('jawaban-soal/{id}', [JawabanSoalController::class, 'destroy']);  
+Route::delete('jawaban-soal/delete/{id}', [JawabanSoalController::class, 'destroy']);  
 
 Route::get('kontribusi-explore', [KontribusiExploreController::class, 'index']);
 Route::post('kontribusi-explore', [KontribusiExploreController::class, 'store']);
 Route::get('kontribusi-explore/{id}', [KontribusiExploreController::class, 'show']);
 Route::put('kontribusi-explore/{id}', [KontribusiExploreController::class, 'update']); 
-Route::delete('kontribusi-explore/{id}', [KontribusiExploreController::class, 'destroy']);  
+Route::delete('kontribusi-explore/delete/{id}', [KontribusiExploreController::class, 'destroy']);  
 
 Route::get('soal-type', [SoalTypeController::class, 'index']);
 Route::post('soal-type', [SoalTypeController::class, 'store']);
 Route::get('soal-type/{id}', [SoalTypeController::class, 'show']);
 Route::put('soal-type/{id}', [SoalTypeController::class, 'update']); 
-Route::delete('soal-type/{id}', [SoalTypeController::class, 'destroy']);  
+Route::delete('soal-type/delete/{id}', [SoalTypeController::class, 'destroy']);  
 
 Route::get('survey', [SurveyController::class, 'index']);
 Route::post('survey', [SurveyController::class, 'store']);
 Route::get('survey/{id}', [SurveyController::class, 'show']);
 Route::put('survey/{id}', [SurveyController::class, 'update']); 
-Route::delete('survey/{id}', [SurveyController::class, 'destroy']);  
+Route::delete('survey/delete/{id}', [SurveyController::class, 'destroy']);  
 
 Route::get('survey-desain', [SurveyDesainController::class, 'index']);
 Route::post('survey-desain', [SurveyDesainController::class, 'store']);
 Route::get('survey-desain/{id}', [SurveyDesainController::class, 'show']);
 Route::put('survey-desain/{id}', [SurveyDesainController::class, 'update']); 
-Route::delete('survey-desain/{id}', [SurveyDesainController::class, 'destroy']);  
+Route::delete('survey-desain/delete/{id}', [SurveyDesainController::class, 'destroy']);  
 
 Route::get('survey-kriteria', [SurveyKriteriaController::class, 'index']);
 Route::post('survey-kriteria', [SurveyKriteriaController::class, 'store']);
 Route::get('survey-kriteria/{id}', [SurveyKriteriaController::class, 'show']);
 Route::put('survey-kriteria/{id}', [SurveyKriteriaController::class, 'update']); 
-Route::delete('survey-kriteria/{id}', [SurveyKriteriaController::class, 'destroy']);  
+Route::delete('survey-kriteria/delete/{id}', [SurveyKriteriaController::class, 'destroy']);  
 
 Route::get('survey-soal', [SurveySoalController::class, 'index']);
 Route::post('survey-soal', [SurveySoalController::class, 'store']);
 Route::get('survey-soal/{id}', [SurveySoalController::class, 'show']);
 Route::put('survey-soal/{id}', [SurveySoalController::class, 'update']); 
-Route::delete('survey-soal/{id}', [SurveySoalController::class, 'destroy']);  
+Route::delete('survey-soal/delete/{id}', [SurveySoalController::class, 'destroy']);  
